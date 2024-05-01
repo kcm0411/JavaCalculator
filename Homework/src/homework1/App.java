302dbd8bd487baaa3ba939e82aa5f19c7fd17e29
Changmin Kim <kcm0411@gmail.com>
2024-05-01 10:31:11 +0900

    1레벨 세번째 요구사항

--- Homework/src/homework1/App.java
+++ Homework/src/homework1/App.java
@@ -9,11 +9,27 @@ public class App {
         System.out.print("첫 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num1 = sc.nextInt();
+
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        String operation = sc.next();
+
         System.out.print("두 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num2 = sc.nextInt();
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-        String operation = sc.next();
+
+        int result = 0;
+        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+        if (operation.equals("+")){
+            result = num1 + num2;
+        } else if (operation.equals("-")){
+            result = num1 - num2;
+        } else if (operation.equals("*")){
+            result = num1 * num2;
+        } else if (operation.equals("/")){
+            result = num1 / num2;
+        }
+
+        System.out.println("결과: " + result);
     }
 }
